01037aa91b870a623bf700a827f55371b5319c6f
Akshit Mangotra <akshit@pool-100-0-242-191.bstnma.fios.verizon.net>
2025-08-19 20:06:35 +0530

    added css for zooming effect

--- src/pages/landing-page/view-season-modal/view-season-modal.tsx
+++ src/pages/landing-page/view-season-modal/view-season-modal.tsx
@@ -26,9 +26,7 @@ export default function ViewSeasonModal({
     <Modal
       isOpen={showModal}
       onClose={() => setShowModal(!showModal)}
-      modalClass={"season-modal-container"}
-      // modalBackgroundColor="black"
-      // overLaybackgroundColor="rgba(0, 0, 0, 0.8)"
+      modalClass={`season-modal-container ${showModal ? "zoom-in" : ""}`}
     >
       {isLoading && <Loader />}
       {!isLoading && (
@@ -38,7 +36,7 @@ export default function ViewSeasonModal({
               className="season-img"
               width={300}
               height={300}
-              src={seasonImg}
+              src={"seasonImg"}
               alt="season-img"
             />
             <div className="season-details-container">
